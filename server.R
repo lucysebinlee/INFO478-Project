e140404b078cb089454c1d7a7c23ba19b941df56
Sebin Lee <sebinlee@D-10-19-7-98.dhcp4.washington.edu>
2019-05-23 10:24:27 -0700

    Update

--- server.R
+++ server.R
@@ -1,1 +1,8 @@
 # INFO 478: Lucy Lee, Rita Wu, Brian Luu, Tabitha Anderson, Katie Clark
+
+# Load Data
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(DT)
--- ui.R
+++ ui.R
@@ -1,1 +1,7 @@
 # INFO 478: Lucy Lee, Rita Wu, Brian Luu, Tabitha Anderson, Katie Clark 
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(DT)
