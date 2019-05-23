252d7ef2e5b69b14a355aec9406220e38a450249
Sebin Lee <sebinlee@D-10-19-7-98.dhcp4.washington.edu>
2019-05-23 11:08:13 -0700

    Update

--- server.R
+++ server.R
@@ -1,6 +1,3 @@
-# INFO 478: Lucy Lee, Rita Wu, Brian Luu, Tabitha Anderson, Katie Clark
-# server.R
-
 # Load Packages
 library(shiny)
 library(ggplot2)
@@ -8,9 +5,18 @@ library(dplyr)
 library(plotly)
 library(DT)
 
-# Load Data
+# Process and Store Data
 d1 = read.table("data/student-mat.csv", sep=";", header = TRUE)
 d2 = read.table("data/student-por.csv", sep=";", header = TRUE)
 d3 = merge(d1,d2,by=c("school", "sex", "age", "address", "famsize", "Pstatus", 
                       "Medu", "Fedu", "Mjob", "Fjob", "reason", "nursery", "internet"))
 print(nrow(d3)) # 382 students
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    
+  })
+  
+})
--- ui.R
+++ ui.R
@@ -1,9 +1,29 @@
-# INFO 478: Lucy Lee, Rita Wu, Brian Luu, Tabitha Anderson, Katie Clark 
-# ui.R
-
 # Load Packages
 library(shiny)
 library(ggplot2)
 library(dplyr)
 library(plotly)
 library(DT)
+
+# Define UI for application
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("bins",
+                   "Number of bins:",
+                   min = 1,
+                   max = 50,
+                   value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))
