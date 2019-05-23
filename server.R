f91d7af578b649c8ec3ab49f58adc6003bc08cd3
Sebin Lee <sebinlee@D-10-19-7-98.dhcp4.washington.edu>
2019-05-23 11:03:35 -0700

    Update

--- server.R
+++ server.R
@@ -1,8 +1,16 @@
 # INFO 478: Lucy Lee, Rita Wu, Brian Luu, Tabitha Anderson, Katie Clark
+# server.R
 
-# Load Data
+# Load Packages
 library(shiny)
 library(ggplot2)
 library(dplyr)
 library(plotly)
 library(DT)
+
+# Load Data
+d1 = read.table("data/student-mat.csv", sep=";", header = TRUE)
+d2 = read.table("data/student-por.csv", sep=";", header = TRUE)
+d3 = merge(d1,d2,by=c("school", "sex", "age", "address", "famsize", "Pstatus", 
+                      "Medu", "Fedu", "Mjob", "Fjob", "reason", "nursery", "internet"))
+print(nrow(d3)) # 382 students
--- ui.R
+++ ui.R
@@ -1,5 +1,7 @@
 # INFO 478: Lucy Lee, Rita Wu, Brian Luu, Tabitha Anderson, Katie Clark 
+# ui.R
 
+# Load Packages
 library(shiny)
 library(ggplot2)
 library(dplyr)
