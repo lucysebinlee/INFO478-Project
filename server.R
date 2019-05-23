cb02cd0c56a5c19f4343256031fd571e00e4a826
Sebin Lee <sebinlee@D-10-19-7-98.dhcp4.washington.edu>
2019-05-23 11:50:49 -0700

    Update

--- server.R
+++ server.R
@@ -6,10 +6,10 @@ library(plotly)
 library(DT)
 
 # Process and Store Data
-d1 = read.table("data/student-mat.csv", sep=";", header = TRUE)
-d2 = read.table("data/student-por.csv", sep=";", header = TRUE)
-d3 = merge(d1,d2,by=c("school", "sex", "age", "address", "famsize", "Pstatus", 
-                      "Medu", "Fedu", "Mjob", "Fjob", "reason", "nursery", "internet"))
+d1 <- data.table::fread("data/student-mat.csv", header = TRUE)
+d2 = data.table::fread("data/student-por.csv", header = TRUE)
+d3 = merge(d1, d2, by = c("school", "sex", "age", "address", "famsize", "Pstatus", 
+                          "Medu", "Fedu", "Mjob", "Fjob", "reason", "nursery", "internet"))
 print(nrow(d3)) # 382 students
 
 # Define server logic required to draw a histogram
